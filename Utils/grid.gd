7072ecce66f9591c0847c6b591d39bec224fcde2
Tiago Santos <tiagowolves87@gmail.com>
2026-03-25 14:54:06 -0300

    adapting movement to grid and making perspective based collisions

--- Utils/grid.gd
+++ Utils/grid.gd
@@ -9,6 +9,9 @@ static func grid_to_world(grid_pos: Vector3) -> Vector3:
 	return world_pos
 
 
-static func world_to_grid(world_pos: Vector3) -> Vector3:
-	var grid_pos: Vector3 = world_pos / tile_size
-	return grid_pos
+static func world_to_grid(world_pos: Vector3) -> Vector3i:
+	return Vector3i(
+		floor(world_pos.x / tile_size.x + 0.5),
+		floor(world_pos.y / tile_size.y + 0.5),
+		floor(world_pos.z / tile_size.z + 0.5)
+	)
