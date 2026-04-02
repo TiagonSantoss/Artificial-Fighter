3df2b7ad4e538a940800f4b8d88a591e4eaec246
Tiago Santos <tiagowolves87@gmail.com>
2026-04-02 19:04:16 -0300

    nothing is working (room generationg)

--- Game/Room/Rooms/Scripts/start.gd
+++ Game/Room/Rooms/Scripts/start.gd
@@ -2,9 +2,6 @@ extends Node3D
 
 func _ready():
 	var builder = GridBuild.new()
-	var grid = builder.build($GridMap)
-	
-	var res = GridData.new()
-	res.grid = grid.grid
-	
-	ResourceSaver.save(res, "res://Game/Room/Rooms/start.tres")
+	var grid: GridData = builder.build($GridMap)
+
+	ResourceSaver.save(grid, "res://Game/Room/Rooms/boss.tres")
